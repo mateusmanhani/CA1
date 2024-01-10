0a1c876c2aa55c6d0f6949d1df0680eb7177bb68
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-01-10 22:05:27 +0000

    Created Student class and Student constructor

--- src/ca1/Student.java
+++ src/ca1/Student.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ca1;
 
 /**
@@ -9,5 +5,17 @@ package ca1;
  * @author Mateus
  */
 public class Student {
+    public String firstName;
+    public String lastName;
+    public String workLoad;
+    public String studentID;
+    public int numClasses;
     
+    public Student(String firstName, String lastName, String studentID, int numClasses){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.numClasses = numClasses;
+        this.workLoad = null;
+    }
 }
