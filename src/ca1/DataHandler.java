423e7e4a5d53a1fe436d83d741be7708ebea49bb
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-01-11 10:39:38 +0000

    Implemented reader on DataHandler class
    created a method to read from a file in order to get the nescessary data to create an student object. Still need to implement validation and store that student in a student repository

--- src/ca1/DataHandler.java
+++ src/ca1/DataHandler.java
@@ -1,9 +1,38 @@
 package ca1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Mateus Manhani
  */
 public class DataHandler {
-    
+    public static void readStudents(){
+        // Use try & catch to try to read the file
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+            String line;
+            
+            // Loop until there are no more lines to read in the file
+            while ((line = reader.readLine()) != null) {
+                //Read first and Last names
+                String[] nameParts = line.split(" ");
+                String firstName = nameParts[0];
+                String lastName = nameParts[1];
+                
+                // Read the number of classes
+                int numClasses = Integer.parseInt(reader.readLine());
+                
+                // Read student ID
+                String studentID = reader.readLine();
+                
+                // Create new student Object
+                Student newStudent = new Student(firstName, lastName, studentID, numClasses);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
