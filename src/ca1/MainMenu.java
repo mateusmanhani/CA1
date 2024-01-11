0a34ddd726c96a4f03f6fee0d01e240ece1c0718
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-01-11 23:35:14 +0000

    implemented takeUserInput method and showMenu method on MainMenu class

---
+++ src/ca1/MainMenu.java
@@ -0,0 +1,94 @@
+
+package ca1;
+
+import java.util.Scanner;
+
+/**
+ * @author Mateus Manhani
+ */
+public class MainMenu {
+    public static void showMenu(){
+        
+        boolean exit = false;
+        
+        
+        while(exit!=true){
+            System.out.println("Welcome to this student application. Read carefully.");
+            System.out.println("1. Read data from local file");
+            System.out.println("2. Manually input data");
+            System.out.println("Please enter the number corresponding to the desired option: ");
+
+            Scanner sc = new Scanner(System.in);
+            int choice = 0;
+            String input = sc.nextLine();
+            
+            sc.close(); //close scanner
+
+            try{
+                choice = Integer.parseInt(input);
+            }catch(Exception e){
+                System.out.println("Not an Integer");
+            }
+            
+            switch (choice){
+                case 1:
+                    // Read data from local file
+                    DataHandler.readStudents();
+                    exit = true;
+                    break;
+                case 2:
+                    //Read data from user Input
+                    
+                default:
+                    System.out.println("Invalid choice.");
+            } 
+        }
+    }
+    
+    public static void takeUserInput(){
+        //Declare nescessary variables
+        String input = null;
+        
+        //Variables for instatiating Student
+        String firstName = null;
+        String lastName = null;
+        String studentID = null;
+        int numClasses = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true){
+            //Take input for fisrt and last name
+            System.out.println("Please enter your first and last name separated by one single space:");
+            input = sc.nextLine();
+            
+            try{
+                //Validate input and save to variables
+                String[] splitName = DataHandler.verifyName(input);
+                firstName = splitName[0];
+                lastName = splitName[1];
+                
+                //Validate and save input for Number of classes
+                System.out.println("Please enter the number of classes 1 through 8:");
+                numClasses = DataHandler.verifyClasses(sc.nextLine());
+                
+                // Validate and save student ID
+                System.out.println("Please enter the student ID: ");
+                studentID = DataHandler.verifyID(sc.nextLine());
+                
+                // If all validation passes, create and add new Student
+                Student newStudent = new Student(firstName, lastName, studentID, numClasses);
+                StudentRepository.addStudent(newStudent);
+                
+                // Ask user  if he wants to add another student
+                System.out.println("Would you like to add another student? (yes/no): ");
+                String addAnother = sc.nextLine().toLowerCase();
+                
+                if (!addAnother.equals("yes")) break; // exit loop if not yes
+            } catch(Exception e){
+                System.out.println("Invalid Input. Please try again.");
+            }  
+        }
+
+    }
+}
